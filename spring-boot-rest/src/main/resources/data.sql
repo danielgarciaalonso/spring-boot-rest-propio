c738664416d5bcd94a157ba4e1bdee4c88555ac5
Daniel Garcia <daniel.garciaalonso@grupocmc.es>
2018-02-21 11:01:03 +0100

    añado un libro

--- spring-boot-rest/src/main/resources/data.sql
+++ spring-boot-rest/src/main/resources/data.sql
@@ -1,1 +1,3 @@
-insert into autor(nombre, direccion) values ('John Papa', 'Angular Street');
+insert into autor(id, nombre, direccion) values (1, 'John Papa', 'Angular Street');
+insert into libro (id, titulo) values (1, 'Mastering AngularJS');
+insert into autor_libro(id_autor, id_libro) values (1, 1);
