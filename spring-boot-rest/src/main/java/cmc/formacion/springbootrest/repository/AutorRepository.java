3bd206cb33cc0889f2a595c4c961dfd1930c3f12
Daniel Garcia <daniel.garciaalonso@grupocmc.es>
2018-02-21 11:04:15 +0100

    Funciones findBy...(String s);

--- spring-boot-rest/src/main/java/cmc/formacion/springbootrest/repository/AutorRepository.java
+++ spring-boot-rest/src/main/java/cmc/formacion/springbootrest/repository/AutorRepository.java
@@ -1,5 +1,7 @@
 package cmc.formacion.springbootrest.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import cmc.formacion.springbootrest.domain.Autor;
 @Repository
 public interface AutorRepository extends JpaRepository<Autor, Long> {
 
+	List<Autor> findByLibrosTitulo(String titulo);
 }
--- spring-boot-rest/src/main/java/cmc/formacion/springbootrest/repository/LibroRepository.java
+++ spring-boot-rest/src/main/java/cmc/formacion/springbootrest/repository/LibroRepository.java
@@ -1,13 +1,16 @@
 package cmc.formacion.springbootrest.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import cmc.formacion.springbootrest.domain.Autor;
 import cmc.formacion.springbootrest.domain.Libro;
 
 @Repository
 public interface LibroRepository extends JpaRepository<Libro, Long> {
 
-	
+	List<Autor> findByTitulo(String titulo);
 	
 }
